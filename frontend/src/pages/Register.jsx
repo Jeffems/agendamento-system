6296ada497aa1839d21d9cb0a2827515a7da07f3
Jeffems <jeferson.sanchina@gmail.com>
2026-02-14 10:31:51 -0400

    fix register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { toast } from "sonner";
 import api from "../services/api";
-import TermsModal from "../components/TermsModal";
+import TermsModal from "../components/termsModal";
 
 export default function Register() {
   const navigate = useNavigate();
