db8f9cd872d5a66cc77c60c0337fbb2f623b2ebe
Jeffems <jeferson.sanchina@gmail.com>
2026-03-01 10:25:22 -0400

    ,

--- frontend/src/pages/Agendamentos.jsx
+++ frontend/src/pages/Agendamentos.jsx
@@ -17,7 +17,7 @@ import {
   LayoutGrid,
   List,
   Calendar,
-  settings,
+  Settings,
 } from "lucide-react";
 import { AnimatePresence } from "framer-motion";
 import { agendamentosAPI } from "../services/api";
@@ -319,7 +319,7 @@ export default function Agendamentos() {
                 WhatsApp
               
               </button>
-              
+
               <button
                 onClick={() => {
                   setAgendamentoEditando(null);
